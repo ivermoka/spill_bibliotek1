51fc0238407ef823ffdb9c5b928183e666824e56
Bobles06 <Elias.gjerde@gmail.com>
2023-01-25 15:33:59 +0100

    dag1ferdig

--- index.php
+++ index.php
@@ -11,5 +11,16 @@ $mysqli = new mysqli(
   $db_password,
   $db_db
 );
+$conn = mysqli_connect($db_host, $db_user, $db_password, $db_db );
 
+if (!$conn){
+    die("connection failed: " . mysqli_connect_error());
+}
+echo "Connection succeessfully";
+
+$sql = "Select * From Brukere";
+
+$result = mysqli_query($conn, $sql);
+
+echo "<td>" . $result;
 ?>
