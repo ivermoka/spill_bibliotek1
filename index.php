c1bc042f665a483394f1db221653a151e3fd097d
Bobles06 <Elias.gjerde@gmail.com>
2023-01-25 15:12:36 +0100

    koblertildatabase

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-$host = ""
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'root';
+$db_db = 'Spillbibliotek1';
+$db_port = 8889;
+$mysqli = new mysqli(
+  $db_host,
+  $db_user,
+  $db_password,
+  $db_db
+);
+
+?>
