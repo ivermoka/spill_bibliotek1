099a0dc2d7f44c7b982e1d3075dc2be52a0187b1
mpu69 <elura001@osloskolen.no>
2023-01-27 11:37:11 +0100

    d

--- app.js
+++ app.js
@@ -6,13 +6,11 @@ let expanded = false;
 
 hamburger.addEventListener("click", () => {
     if (expanded === false) {
-        dropdown.style.display = "flex"
-        dropdown.classList.add("show")
-        expanded = true
+        dropdown.style.display = "flex";
+        dropdown.style.transform =" translateX(0%)"
+        expanded = true;
+    } else if (expanded === true) {
+        dropdown.style.display = "none";
+        expanded = false;
     }
-    else if (expanded === true) {
-        dropdown.style.display = "none"
-        dropdown.classList.remove("show")
-        expanded = false
-    }
-});
+}); 
