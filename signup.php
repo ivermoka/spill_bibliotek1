8118ff70848a513ff1eb19d291b9745cfc5fe75c
Bobles06 <Elias.gjerde@gmail.com>
2023-02-01 14:52:56 +0100

    moren din

--- signup.php
+++ signup.php
@@ -18,50 +18,12 @@
   <body>
     <?php
 
-      $db_host = 'localhost';
-      $db_user = 'root';
-      $db_password = 'root';
-      $db_db = 'Spillbibliotek1';
-      $db_port = 8888;
-      $mysqli = new mysqli(
-        $db_host,
-        $db_user,
-        $db_password,
-        $db_db
-      );
-      $conn = mysqli_connect($db_host, $db_user, $db_password, $db_db );
-      
-      if (!$conn){
-          die("connection failed: " . mysqli_connect_error());
-      }
-      echo "Connection succeessfully";
-      
-      
-      $sql = "DELETE FROM Brukere WHERE BrukerID = 2";
-      $result = mysqli_query($conn, $sql) 
-      
-      $sql_fetch_things = "SELECT * FROM Brukere";
-      
-      $result = mysqli_query($conn, $sql_fetch_things);
-      while( $row = $result->fetch_array()){
-        //print_r($row);
-        echo "<br />";
-        echo "BrukerID: " . $row["BrukerID"] . "<br />";
-        echo "Brukernavn: " . $row["Brukernavn"] . "<br />";
-        echo "Passord: " . $row["Passord"] . "<br />";
-        echo "Email: " . $row["Email"] . "<br />";
-      }
-      
-      
-      echo "<td>" . $result;
       ?>
-    <form method="post">
-      <input
-        type="submit"
-        name="button1"
-        class="button1 bg-white"
-        value="Button1"
-      />
+    <form action="index.php" method = "post">
+        Name:<input type="text" name="name"><br>
+        Passord:<input type="text" name="passord"><br>
+        <input type ="submit">
+
     </form>
     <div class="inset-x-0 top-0"></div>
   </body>
