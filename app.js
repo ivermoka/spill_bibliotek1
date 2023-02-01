947cf72282e97e62cc9cf87f97032657f58f89c6
ivermoka <ivermoka@icloud.com>
2023-02-01 15:00:13 +0100

    iver

--- app.js
+++ app.js
@@ -26,3 +26,63 @@ hamburger.addEventListener("click", () => {
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
 });
+
+const buttonPlaystation = document.querySelector("#playstation");
+const dropdownPlaystation = document.querySelector("#dropdownPlaystation");
+let expandedPlaystation = false;
+
+buttonPlaystation.addEventListener("click", () => {
+  if (!expandedPlaystation) {
+    dropdownPlaystation.style.display = "flex";
+    buttonPlaystation.style.padding = "16px";
+    expandedPlaystation = true;
+  } else {
+    dropdownPlaystation.style.display = "none";
+    expandedPlaystation = false;
+  }
+});
+
+buttonPlaystation.addEventListener("click", () => {
+  buttonPlaystation.classList.toggle("active");
+});
+
+const buttonXbox = document.querySelector("#xbox");
+const dropdownXbox = document.querySelector("#dropdownXbox");
+let expandedXbox = false;
+
+buttonXbox.addEventListener("click", () => {
+  console.log()
+  if (!expandedXbox) {
+    dropdownXbox.style.display = "flex";
+    buttonXbox.style.padding = "16px";
+    expandedXbox = true;
+  } else {
+    dropdownXbox.style.display = "none";
+    expandedXbox = false;
+  }
+});
+
+buttonXbox.addEventListener("click", () => {
+  buttonXbox.classList.toggle("active");
+});
+
+
+const buttonNin = document.querySelector("#nintendo");
+const dropdownNin = document.querySelector("#dropdownNin");
+let expandedNin = false;
+
+buttonNin.addEventListener("click", () => {
+  console.log()
+  if (!expandedNin) {
+    dropdownNin.style.display = "flex";
+    buttonNin.style.padding = "16px";
+    expandedNin = true;
+  } else {
+    dropdownNin.style.display = "none";
+    expandedNin = false;
+  }
+});
+
+buttonNin.addEventListener("click", () => {
+  buttonNin.classList.toggle("active");
+});
